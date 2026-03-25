4fc7e3a21fc14e2273082beab6275103646df11c
Andrew Blodgett <61248022+andrewblodgett@users.noreply.github.com>
2026-03-25 14:35:23 -0600

    Add documentation for chart color and X-axis label functions
    Added JSDoc comments for setChartColor and setXAxisLabels functions to improve documentation.

--- default.sb4config/charts.js
+++ default.sb4config/charts.js
@@ -1,15 +1,25 @@
+/**
+ * Updates the color palette of a specified SmartBuilder chart widget.
+ * * @requires utilities.js
+ * @param {string|Object} chartName - The name of the chart widget or the widget object itself.
+ * @param {Array<string>} colors - An array of CSS color strings (e.g., ['#FF0000', 'blue', 'rgb(0, 255, 0)']) to apply to the chart data.
+ */
 function setChartColor(chartName, colors) {
-    let chart = getSBWidgetFromObject(chartName);
-    chart.option.color = colors;
-    chart.chart.setOption(chart.option);
-
+    let chart = getSBWidgetFromObject(chartName);
+    chart.option.color = colors;
+    chart.chart.setOption(chart.option);
 }
 SB4API.functions.setChartColor = setChartColor
 
+/**
+ * Replaces the existing X-axis labels on a specified SmartBuilder chart widget.
+ * * @requires utilities.js
+ * @param {string|Object} chartName - The name of the chart widget or the widget object itself.
+ * @param {Array<string>} labels - An array of strings representing the new X-axis categories (e.g., ['Jan', 'Feb', 'Mar']).
+ */
 function setXAxisLabels(chartName, labels) {
-    let chart = getSBWidgetFromObject(chartName);
-    chart.option.xAxis.data = labels;
-    chart.chart.setOption(chart.option);
-
+    let chart = getSBWidgetFromObject(chartName);
+    chart.option.xAxis.data = labels;
+    chart.chart.setOption(chart.option);
 }
 SB4API.functions.setXAxisLabels = setXAxisLabels
